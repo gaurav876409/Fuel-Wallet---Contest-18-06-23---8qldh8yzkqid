e47fa92d362884450513b872bde10c56d61bfbaf
gaurav876409 <gaurav876409@gmail.com>
2023-06-18 13:52:35 +0530

    up

--- src/reducers.js
+++ src/reducers.js
@@ -2,13 +2,13 @@ import { v4 as uuidv4 } from "uuid";
 
 const reducer = (state, action) => {
     if(action.type === "add"){
-        action.setbalance((balance) => {
+        action.setBalance((balance) => {
             return balance + 10;
         });
         return[...state, {name: action.name, id: uuidv4()}];
     }else if(action.type === "fuel"){
         if(action.balance>=50){
-            action.setbalance((balance) => balance-50);
+            action.setBalance((balance) => balance-50);
         }
         return state;
     }else {
